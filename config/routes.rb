6e5183d5f12e308d26fd36ec4514fedcbc86a35a
shin30914 <sakatasandan@gmail.com>
2018-11-17 01:08:02 +0900

    add devise controller customize

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users, controllers: {
       omniauth_callbacks: 'users/omniauth_callbacks',
+      registrations: 'users/registrations',
+      sessions: 'users/sessions',
   }
-  # devise_scope :user do
-  #   root to: 'devise/sessions#new'
-  # end
 end
